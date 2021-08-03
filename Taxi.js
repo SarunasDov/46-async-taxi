cf51af051c033fb94534d256eec188eaa06e16ad
Sarunas <sar.dovydaitis@hotmail.com>
2021-08-03 20:04:58 +0300

    taxi done

---
+++ Taxi.js
@@ -0,0 +1,25 @@
+class Taxi {
+    constructor() {
+
+    }
+
+    drive(distance) {
+        let currentDistance = 0;
+
+        return new Promise((resolve, reject) => {
+            const timer = setInterval(() => {
+                console.log(`Nuvaziuota: ${++currentDistance} km.`);
+
+                if (currentDistance === distance) {
+                    clearInterval(timer);
+                    console.log('Kelione baigta.');
+                    resolve();
+                }
+
+            }, 1000);
+        })
+    }
+
+}
+
+module.exports = Taxi;
